438803d3e9fc0391181e12df8ad648cd2669ab94
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2020-05-27 01:02:19 -0400

    primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,58 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if(!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
+};
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+};
+
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuarios conectados',resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+//socket.emit('crearMensaje', {
+ //   nombre: 'Violeta',
+   // mensaje: 'Bienvenidos al chat'
+//}, function(resp) {
+  //  console.log('respuesta server: ', resp);
+//});
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+// Escuchar cambios de usuarios
+//cuando un usuario entra o sale del chat 
+socket.on('crearMensaje', function(usuarios) {
+
+    console.log('Servidor:', usuarios);
+
+});
+
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado:', mensaje);
+});
